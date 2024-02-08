cc98892a118ba3529768a34a388ca04abcfd460f
dimabkst <dimabkst@gmail.com>
2024-02-08 19:14:03 +0200

    draw histogram function

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ from traceback import print_exc
 from scipy.stats import chi2, rv_continuous
 import matplotlib.pyplot as plt
 
-# Variant - 1
-# implemented function only for continuous probability distributions
-def labTask(*distributionShapeParams, n: int=800, probabilityDistribution: rv_continuous=chi2):
-    sample = probabilityDistribution.rvs(*distributionShapeParams, size=n)
-
+def drawHistogram(data, *distributionShapeParams):
     fig, ax = plt.subplots(1, 1)
 
-    ax.hist(sample, density=True, bins='auto', histtype='stepfilled', color='g')
+    ax.hist(data, density=True, bins='auto', histtype='stepfilled', color='g')
 
     ax.set_title('Гістограма вибірки')
 
@@ -25,10 +21,17 @@ def labTask(*distributionShapeParams, n: int=800, probabilityDistribution: rv_co
 
     plt.show()
 
+# Variant - 1
+# implemented function only for continuous probability distributions
+def labTask(*distributionShapeParams, n: int=800, probabilityDistribution: rv_continuous=chi2):
+    sample = probabilityDistribution.rvs(*distributionShapeParams, size=n)
+
+    drawHistogram(sample, *distributionShapeParams)
+
 
 if __name__ == '__main__':
     try:
-        labTask(7)
+        labTask(7, probabilityDistribution=chi2, n=800)
     except Exception as e:
         print('Error occurred:')
         print_exc()
